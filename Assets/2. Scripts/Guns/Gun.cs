6ae6c1a7f7bcc3da19a1a1527fa169e49f5504c6
Folgui12 <folguerasbenjamin@gmail.com>
2025-04-24 10:18:08 -0300

    Trying to adjust the bullet orientation

--- Assets/2. Scripts/Guns/Gun.cs
+++ Assets/2. Scripts/Guns/Gun.cs
@@ -6,12 +6,12 @@ public class Gun : MonoBehaviour
 {
     public GameObject bulletPrefab;   // The bullet prefab to instantiate
     public Transform firePoint;       // Where the bullets spawn from
-    public float bulletForce = 20f;   // Speed of the bullet
+    public float bulletForce = 2f;   // Speed of the bullet
 
     public void Shoot()
     {
         // Create the bullet at the firePoint's position and rotation
-        GameObject bullet = Instantiate(bulletPrefab, firePoint.position, firePoint.rotation);
+        GameObject bullet = Instantiate(bulletPrefab, firePoint.position, firePoint.transform.rotation);
 
         // Get the Rigidbody component from the bullet to apply force
         Rigidbody rb = bullet.GetComponent<Rigidbody>();
